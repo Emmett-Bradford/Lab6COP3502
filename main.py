0db9b2a3a1513f2e119ecb9ea9391bac6b2b4cef
asingh300 <asingh4@ufl.edu>
2023-10-25 02:33:10 -0400

    decode function added and menu output for option 2

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ def encode(to_encode):
   return string
 
 def decode(to_decode):
-  pass
+  string = ''
+  for i in range(len(to_decode)):
+    string += str((int(to_decode[i])-3)%10)
+  return string
 
 if __name__ == '__main__':
   passw = None
+  dpassw = None
   while True:
     modenum = input('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n\nPlease enter an option: ')
     if modenum == '1':
       passw = encode(input("Please enter your password to encode: "))
       print("Your password has been encoded and stored!\n")
     elif modenum == '2':
-      pass
-      #Put decode function here
+      try:
+        dpassw = decode(passw)
+        print(f"The encoded password is {passw}, and the original password is {dpassw}")
+      except:
+        print("You need to encode a password first!")
     elif modenum == '3':
       break
